a8ac1c053a78ec5355511c0217adb98e00df8a7f
Amy Kang <selene6023@gmail.com>
2014-11-30 16:08:08 -0800

    Added Devise

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,4 +1,5 @@
 class RepliesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_reply, only: [:show, :edit, :update, :destroy]
   before_action :set_post
 
