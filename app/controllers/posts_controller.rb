8789b4da8f3b5d0bc85695815e8c3c6e64c8a369
Amy Kang <selene6023@gmail.com>
2014-11-30 17:57:58 -0800

    Fixed validation error in Post Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action :check_user, except: [:index, :show, :destroy]
+  before_action :check_user, except: [:index, :show, :destroy, :new, :create]
   before_action :set_admin, only: [:destroy]
 
   respond_to :html
